6d2c99765b694755c39c11906373073a0f5cab05
iskandarsamsuddin@gmail.com <mohdiskandarbinsamsuddin@Mohds-MacBook-Pro.local>
2021-03-15 23:25:25 +0800

    add seo
    Signed-off-by: iskandarsamsuddin@gmail.com <mohdiskandarbinsamsuddin@Mohds-MacBook-Pro.local>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SeoService } from './services/seo.service';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
+import { filter, map, mergeMap, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -17,4 +20,23 @@ export class AppComponent {
     { path: 'assets/images/feedback-6.jpeg' },
     { path: 'assets/images/feedback-7.jpeg' }
   ];
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute, private seoService: SeoService) { }
+
+  ngOnInit(): void {
+    this.router.events.pipe(
+      filter(e => e instanceof NavigationEnd),
+      map(e => this.activatedRoute),
+      map((route) => {
+        while (route.firstChild) route = route.firstChild;
+        return route;
+      }),
+      filter((route) => route.outlet === 'primary'),
+      mergeMap((route) => route.data),
+    ).subscribe(data => {
+      let seoData = data['seo'];
+      this.seoService.updateTitle(seoData['title']);
+      this.seoService.updateMetaTags(seoData['metaTags']);
+    });
+  }
 }
